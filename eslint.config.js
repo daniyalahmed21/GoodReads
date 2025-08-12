f733be3f18650ff6ffdb104441dcff19e3930e91
Daniyal Ahmed <da9033431@gmail.com>
2025-08-12 11:20:30 +0100

    eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -22,8 +23,32 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    // Add the plugins section
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // Add the simple-import-sort rule with your custom groups
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // 1. Side effect imports
+            ['^\\u0000'],
+            // 2. React and packages
+            ['^react$', '^@?\\w'],
+            // 3. Absolute imports and other imports
+            ['^@', '^'],
+            // 4. Relative imports from the same folder "./"
+            ['^\\./'],
+            // 5. Style module imports
+            ['^.+\\.(module.css|module.scss)$'],
+            // 6. Media imports
+            ['^.+\\.(gif|png|svg|jpg)$'],
+          ],
+        },
+      ],
     },
   },
-])
+]);
